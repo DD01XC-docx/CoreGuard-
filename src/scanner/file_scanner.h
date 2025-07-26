3026eb8bb2284fb23c2e6a559e49d5df7a60b9c9
YEV <nentisoff@gmail.com>
2025-07-26 17:01:25 +0200

    Update file_scanner.h
    Implement Scanner class with isMalicious check based on file hash.

--- src/scanner/file_scanner.h
+++ src/scanner/file_scanner.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <string>
+#include <vector>
 
+class Scanner {
+public:
+	Scanner(const std::vector <std:string>& known_signatures);
+	bool isMalicious(const std::string& file_path);
+
+private:
+	std::vector<std::string> signatures;
+
+	// SHA256 hash of the file
+	std::string hashFile(const std::string& file_path);
+};
