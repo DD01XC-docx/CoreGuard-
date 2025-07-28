eefd38a3bbe2aa458fc21eb7ae7d27737fda5feb
YEV <nentisoff@gmail.com>
2025-07-28 14:24:04 +0200

    Update signature_db.cpp
    namespace signature_db

--- src/signatures/signature_db.cpp
+++ src/signatures/signature_db.cpp
@@ -1,1 +1,15 @@
+#include "signature_db.h"
+#include <fstream>
 
+std::vector<std::string> loadSignatures(const std::string& filepath) {
+	std::vector<std::string> signatures;
+	std::ifstream file(filepath);
+	std::string line;
+
+	while (std::getline (file, line)) {
+		if (!line.empty()) {
+			signatures.push_back(line);
+		}
+		return signatures;
+	}
+} // namespace signature_db
